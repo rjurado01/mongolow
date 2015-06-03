606d2123969e2afa8002a4c996d1e7ce391eb64e
rjurado <rjurado@openmailbox.org>
2015-06-03 20:29:02 +0200

    Added drop_table to driver.

--- lib/mongolow/driver.rb
+++ lib/mongolow/driver.rb
@@ -6,11 +6,17 @@ module Mongolow
   class Driver
     class << self
       def initialize(ip, port, database_name)
-        @session = MongoClient.new( ip, port ).db( database_name )
+        @database_name = database_name
+        @client = MongoClient.new( ip, port )
+        @session = @client.db( database_name )
       end
 
       def session
         @session
+      end
+
+      def drop_database
+        @client.drop_database(@database_name)
       end
     end
   end
--- spec/lib/driver_spec.rb
+++ spec/lib/driver_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe "Mongolow::Driver" do
-  context "when call session method" do
-    it "return session instance" do
+  describe "session" do
+    it "returns session instance" do
       session = Mongolow::Driver.initialize('127.0.0.1', 27017, 'mongolow_test')
       expect(Mongolow::Driver.session).to eq(session)
+    end
+  end
+
+  describe "drop_database" do
+    it "drops database" do
+      session = Mongolow::Driver.initialize('127.0.0.1', 27017, 'mongolow_test')
+      session['my_model'].insert({name: 'name1'})
+      Mongolow::Driver.drop_database
+      expect(session.collection_names).to eq([])
     end
   end
 end
